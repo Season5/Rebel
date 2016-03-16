d555021149ea3122d85f757462f1fbfcf1aa968a
Onikah <mona.kwamboka@gmail.com>
2016-03-16 12:39:29 +0300

    Link to youtube video rectified

--- app/src/main/java/com/example/jaykayitare/rebel/MainActivity.java
+++ app/src/main/java/com/example/jaykayitare/rebel/MainActivity.java
@@ -176,7 +176,7 @@ public class MainActivity extends ActionBarActivity implements ViewAnimator.View
                 Intent intent2 = new Intent(getApplicationContext(),AudioActivity.class);
                 startActivity(intent2);
             case ContentFragment.VIDEO:
-                Intent intent3 = new Intent(getApplicationContext(),YouTubeActivity.class);
+                Intent intent3 = new Intent(this,VideoActivity.class);
                 startActivity(intent3);
             case ContentFragment.BUY:
                 Intent intent4 = new Intent(getApplicationContext(),AudioActivity.class);
