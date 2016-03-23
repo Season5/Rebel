345b3b7d99f8aa19b55e282cba75d58d56180b77
jaykayitare <jmvkayitare@yahoo.com>
2016-03-23 12:28:39 +0300

    Removed Drawer menu

--- app/src/main/java/com/example/jaykayitare/rebel/MainActivity.java
+++ app/src/main/java/com/example/jaykayitare/rebel/MainActivity.java
@@ -15,6 +15,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.example.jaykayitare.rebel.fragment.AudioFragment;
@@ -30,177 +31,51 @@ import yalantis.com.sidemenu.interfaces.ScreenShotable;
 import yalantis.com.sidemenu.model.SlideMenuItem;
 import yalantis.com.sidemenu.util.ViewAnimator;
 
-public class MainActivity extends ActionBarActivity implements ViewAnimator.ViewAnimatorListener {
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle drawerToggle;
-    private List<SlideMenuItem> list = new ArrayList<>();
-    private ContentFragment contentFragment;
-    private ViewAnimator viewAnimator;
-    private int res = R.id.fragment_content;
-    private LinearLayout linearLayout;
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
+
+    Button about, buy, audio, video;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        contentFragment = ContentFragment.newInstance(res);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.content_frame, contentFragment)
-                .addToBackStack(null)
-                .commit();
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerLayout.setScrimColor(Color.TRANSPARENT);
-        linearLayout = (LinearLayout) findViewById(R.id.left_drawer);
-        linearLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                drawerLayout.closeDrawers();
-            }
-        });
 
+        about = (Button)findViewById(R.id.aboutbutton);
+        buy = (Button)findViewById(R.id.buybutton);
+        audio = (Button)findViewById(R.id.audiobutton);
+        video = (Button)findViewById(R.id.videobutton);
 
-        setActionBar();
-        createMenuList();
-        viewAnimator = new ViewAnimator<>(this, list, contentFragment, drawerLayout, this);
-    }
+        about.setOnClickListener(this);
+        buy.setOnClickListener(this);
+        audio.setOnClickListener(this);
+        video.setOnClickListener(this);
 
-    private void createMenuList() {
-        SlideMenuItem menuItemClose = new SlideMenuItem(ContentFragment.CLOSE, R.drawable.icn_close);
-        list.add(menuItemClose);
-        SlideMenuItem menuItemDashboard = new SlideMenuItem(ContentFragment.DASHBOARD, R.drawable.ic_dashboard_black_48dp);
-        list.add(menuItemDashboard);
-        SlideMenuItem menuItemAudio = new SlideMenuItem(ContentFragment.AUDIO, R.drawable.ic_music_note_black_48dp);
-        list.add(menuItemAudio);
-        SlideMenuItem menuItemVideo = new SlideMenuItem(ContentFragment.VIDEO, R.drawable.ic_music_video_black_48dp);
-        list.add(menuItemVideo);
-        SlideMenuItem menuItemBuy = new SlideMenuItem(ContentFragment.BUY, R.drawable.red_buynow);
-        list.add(menuItemBuy);
-        SlideMenuItem menuItemAbout = new SlideMenuItem(ContentFragment.ABOUT, R.drawable.rebel);
-        list.add(menuItemAbout);
-    }
-
-
-    private void setActionBar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        drawerToggle = new ActionBarDrawerToggle(
-                this,                  /* host Activity */
-                drawerLayout,         /* DrawerLayout object */
-                toolbar,  /* nav drawer icon to replace 'Up' caret */
-                R.string.drawer_open,  /* "open drawer" description */
-                R.string.drawer_close  /* "close drawer" description */
-        ) {
-
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                linearLayout.removeAllViews();
-                linearLayout.invalidate();
-            }
-
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
-                super.onDrawerSlide(drawerView, slideOffset);
-                if (slideOffset > 0.6 && linearLayout.getChildCount() == 0)
-                    viewAnimator.showMenuContent();
-            }
-
-            /** Called when a drawer has settled in a completely open state. */
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-            }
-        };
-        drawerLayout.addDrawerListener(drawerToggle);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        drawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        drawerToggle.onConfigurationChanged(newConfig);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private ScreenShotable replaceFragment(ScreenShotable screenShotable, int topPosition) {
-//        this.res = this.res == R.id.fragment_content ? R.id.audio_content : R.id.videofragment;
-        View view = findViewById(R.id.content_frame);
-        int finalRadius = Math.max(view.getWidth(), view.getHeight());
-        SupportAnimator animator = ViewAnimationUtils.createCircularReveal(view, 0, topPosition, 0, finalRadius);
-        animator.setInterpolator(new AccelerateInterpolator());
-        animator.setDuration(ViewAnimator.CIRCULAR_REVEAL_ANIMATION_DURATION);
-
-        findViewById(R.id.content_overlay).setBackground(new BitmapDrawable(getResources(), screenShotable.getBitmap()));
-        animator.start();
-        ContentFragment contentFragment = ContentFragment.newInstance(this.res);
-        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, contentFragment).commit();
-        return contentFragment;
-    }
-
-    @Override
-    public ScreenShotable onSwitch(Resourceble slideMenuItem, ScreenShotable screenShotable, int position) {
-
-        switch (slideMenuItem.getName()) {
-            case ContentFragment.CLOSE:
-                return screenShotable;
-            case ContentFragment.ABOUT:
-                Intent intent = new Intent(getBaseContext(),AboutActivity.class);
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buybutton:
+                intent = new Intent(this, BuyActivity.class);
                 startActivity(intent);
-            case ContentFragment.AUDIO:
-                Intent intent2 = new Intent(getBaseContext(),AudioActivity.class);
-                startActivity(intent2);
-            case ContentFragment.VIDEO:
-                Intent intent3 = new Intent(this,VideoActivity.class);
-                startActivity(intent3);
-            case ContentFragment.BUY:
-                Intent intent4 = new Intent(getBaseContext(),BuyActivity.class);
-                startActivity(intent4);
+                break;
+            case R.id.aboutbutton:
+                intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.videobutton:
+                intent = new Intent(this, YouTubeActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.audiobutton:
+                intent = new Intent(this, AudioActivity.class);
+                startActivity(intent);
+                break;
             default:
-                return screenShotable;
+                break;
         }
-    }
-
-    @Override
-    public void disableHomeButton() {
-        getSupportActionBar().setHomeButtonEnabled(false);
-
-    }
-
-    @Override
-    public void enableHomeButton() {
-        getSupportActionBar().setHomeButtonEnabled(true);
-        drawerLayout.closeDrawers();
-
-    }
-
-    @Override
-    public void addViewToContainer(View view) {
-        linearLayout.addView(view);
     }
 }
